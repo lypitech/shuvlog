6b3a1d3f12554a29ced67dfaddaa610b03470b09
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-09 23:22:00 +0100

    feat(timestamp): Added `formatTimestamp` function.

--- include/logger/Timestamp.h
+++ include/logger/Timestamp.h
@@ -6,5 +6,6 @@
 using namespace std::chrono;
 
 std::tm fromTimePoint(time_point<system_clock> timestamp);
+std::string formatTimestamp(time_point<system_clock> timestamp, bool forFilename = false);
 
 #endif //SHOVOLOGGER_TIMESTAMP_H
--- src/Timestamp.cpp
+++ src/Timestamp.cpp
@@ -1,6 +1,7 @@
 #include "logger/Timestamp.h"
 
 #include <chrono>
+#include <iomanip>
 
 std::tm fromTimePoint(time_point<system_clock> timestamp)
 {
@@ -9,4 +10,20 @@ std::tm fromTimePoint(time_point<system_clock> timestamp)
 
     localtime_r(&t, &tm);
     return tm;
+}
+
+std::string formatTimestamp(time_point<system_clock> timestamp, bool forFilename)
+{
+    const std::tm tm = fromTimePoint(timestamp);
+    std::ostringstream oss;
+
+    if (forFilename) {
+        oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+    } else {
+        const auto ms = duration_cast<milliseconds>(timestamp.time_since_epoch()) % 1000;
+
+        oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S")
+            << '.' << std::setw(3) << std::setfill('0') << ms.count();
+    }
+    return oss.str();
 }
