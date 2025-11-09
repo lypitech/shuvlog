27643a2691908e4b77d58b5bf42a20b9d7080a9b
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-09 23:22:58 +0100

    feat: Added `Sink` interface.

---
+++ include/logger/Sink.h
@@ -0,0 +1,30 @@
+#ifndef SHOVOLOGGER_SINK_H
+#define SHOVOLOGGER_SINK_H
+
+#include <string>
+
+#include "Log.h"
+#include "Settings.h"
+
+namespace logger
+{
+
+class Sink
+{
+public:
+    virtual ~Sink() = default;
+
+    virtual void write(const Log& log, const Settings& settings) = 0;
+    virtual void writeHeader(
+        const std::string& projectName,
+        int argc,
+        const char* argv[],
+        const Settings& settings
+    ) = 0;
+    virtual void flush() = 0;
+    virtual void close() = 0;
+};
+
+}
+
+#endif //SHOVOLOGGER_SINK_H
