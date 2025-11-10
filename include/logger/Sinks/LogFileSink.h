0618b69e5b03a3f167654e29964160ee5c08f99f
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-10 09:28:55 +0100

    feat(sink): Added `LogFile` sink.

---
+++ include/logger/Sinks/LogFileSink.h
@@ -0,0 +1,32 @@
+#ifndef SHOVOLOGGER_LOGFILESINK_H
+#define SHOVOLOGGER_LOGFILESINK_H
+
+#include <fstream>
+
+#include "logger/Sink.h"
+
+namespace logger
+{
+
+class LogFileSink : public Sink
+{
+public:
+    explicit LogFileSink(const std::string& filepath);
+
+    void write(const Log& log, const Settings& settings) override;
+    void writeHeader(
+        const std::string &projectName,
+        int argc,
+        const char *argv[],
+        const Settings &settings
+    ) override;
+    void flush() override;
+    void close() override;
+
+private:
+    std::ofstream _file;
+};
+
+}
+
+#endif //SHOVOLOGGER_LOGFILESINK_H
---
+++ src/Sinks/LogFileSink.cpp
@@ -0,0 +1,86 @@
+#include "logger/Sinks/LogFileSink.h"
+
+#include "logger/Logger.h"
+#include "logger/OsInfo.h"
+#include "logger/Timestamp.h"
+#include "logger/Exceptions/LoggerException.h"
+
+namespace logger
+{
+
+LogFileSink::LogFileSink(const std::string &filepath)
+    : _file(filepath, std::ios::app)
+{
+    if (!_file.is_open()) {
+        throw exception::LoggerException(std::format("{}: Could not open file.", filepath));
+    }
+}
+
+static std::string formatLog(
+    const Log& log,
+    const Settings& settings
+)
+{
+    std::string ts;
+    if (settings.getShowTimestamp()) {
+        ts = "[" + formatTimestamp(log.getTimestamp()) + "] ";
+    }
+
+    std::string src;
+    if (settings.getShowSource()) {
+        src = std::format(
+            " ({}:{})",
+            log.getLocation().file_name(), log.getLocation().line()
+        );
+    }
+
+    return std::format(
+        "{}<{}> {}{}\n",
+        ts, Logger::levelToString(log.getLevel()),
+        log.getMessage(), src
+    );
+}
+
+void LogFileSink::write(
+    const Log& log,
+    const Settings& settings
+)
+{
+    _file << formatLog(log, settings);
+}
+
+void LogFileSink::writeHeader(
+    const std::string &projectName,
+    int argc,
+    const char *argv[],
+    const Settings &settings
+)
+{
+    std::ostringstream header;
+
+    header << "=== LOG START: " << formatTimestamp(system_clock::now()) << " ===\n";
+    header << "Project: " << projectName << "\n";
+    header << "Command: ";
+    for (int i = 0; i < argc; i++) {
+        header << argv[i] << (i + 1 == argc ? "" : " ");
+    }
+    header << "\n";
+    header << "OS: " << osname() << " " << kernelver() << "\n";
+    header << "Minimum log level: " << Logger::levelToString(settings.getMinimumLevel()) << "\n\n";
+
+    _file << header.str();
+}
+
+void LogFileSink::flush()
+{
+    _file.flush();
+}
+
+void LogFileSink::close()
+{
+    if (_file.is_open()) {
+        _file.close();
+    }
+}
+
+}
