f32c8a38b77bc788742c626c9f62cd3e1de318eb
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-14 13:43:56 +0100

    feat: Added thread info (ID and name) in logs.

--- include/logger/Log.h
+++ include/logger/Log.h
@@ -1,6 +1,8 @@
 #ifndef SHOVOLOGGER_LOG_H
 #define SHOVOLOGGER_LOG_H
 
+#include <thread>
+#include <chrono>
 #include <source_location>
 
 #include "Level.h"
@@ -17,12 +19,16 @@ public:
     [[nodiscard]] std::string getMessage() const { return _message; }
     [[nodiscard]] logger::Level getLevel() const { return _level; }
     [[nodiscard]] std::source_location getLocation() const { return _location; }
+    [[nodiscard]] std::thread::id getThreadId() const { return _threadId; }
+    [[nodiscard]] std::string getThreadName() const { return _threadName; }
     [[nodiscard]] std::chrono::time_point<std::chrono::system_clock> getTimestamp() const { return _timestamp; }
 
 private:
     std::string _message;
     logger::Level _level;
     std::source_location _location;
+    std::thread::id _threadId;
+    std::string _threadName;
     std::chrono::time_point<std::chrono::system_clock> _timestamp;
 };
 
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,4 +1,5 @@
 #include "logger/Logger.h"
+#include "logger/Thread.h"
 
 Log::Log(
     std::string message,
@@ -8,5 +9,7 @@ Log::Log(
     : _message(std::move(message))
     , _level(level)
     , _location(loc)
+    , _threadId(std::this_thread::get_id())
+    , _threadName(logger::getThreadLabel())
     , _timestamp(std::chrono::system_clock::now())
 {}
