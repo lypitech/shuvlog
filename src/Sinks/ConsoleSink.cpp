b093fa921910f827025d5a5257801289eaf7df68
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-14 13:44:52 +0100

    feat(sink>console): Updated output format.
    - Removed messy and ugly code
    - Added thread ID and thread name
    - Added width to type formatting

--- src/Sinks/ConsoleSink.cpp
+++ src/Sinks/ConsoleSink.cpp
@@ -10,46 +10,20 @@ namespace logger
 
 static std::string formatLog(
     const Log& log,
-    const Settings& settings
+    const Settings& /*settings*/
 )
 {
-    const bool isOutputColored = settings.getColoredOutput();
-    std::string prefix;
+    std::ostringstream oss;
+    oss << log.getThreadId();
 
-    if (settings.getShowTimestamp()) {
-        prefix += "[" + formatTimestamp(log.getTimestamp()) + "] ";
-    }
-    if (isOutputColored) {
-        switch (log.getLevel()) {
-            case Level::kDebug:     prefix += "\033[34m";       break; // blue
-            case Level::kWarning:   prefix += "\033[33m";       break; // yellow
-            case Level::kError:
-            case Level::kCritical:  prefix += "\033[31m";       break; // red
-            case Level::kFatal:     prefix += "\033[41;97m";    break; // red BG white text
-            default:                                            break;
-        }
-    }
-
-    prefix += std::format(
-        "<{}> ",
-        Logger::levelToString(log.getLevel())
+    return std::format(
+        "{} [{} ({})] {:>8}: {} ({}:{})\n", // 8 is "CRITICAL"'s length (longest type)
+        formatTimestamp(log.getTimestamp()),
+        log.getThreadName(), oss.str(),
+        Logger::levelToString(log.getLevel()),
+        log.getMessage(),
+        log.getLocation().file_name(), log.getLocation().line()
     );
-
-    std::string source;
-    if (settings.getShowSource()) {
-        source = std::format(
-            " ({}:{})",
-            log.getLocation().file_name(), log.getLocation().line()
-        );
-    }
-
-    std::string output = std::format(
-        "{}{}{}{}\n",
-        prefix, log.getMessage(), source,
-        isOutputColored ? "\033[0m" : ""
-    );
-
-    return output;
 }
 
 void ConsoleSink::write(const Log& log, const Settings& settings)
@@ -59,7 +33,7 @@ void ConsoleSink::write(const Log& log, const Settings& settings)
             ? std::cerr
             : std::cout;
 
-    std::string output = formatLog(log, settings);
+    const std::string output = formatLog(log, settings);
 
     out << output;
 }
