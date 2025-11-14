7a6f78bce6d739dcf81785fa0437732944e9c811
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-14 11:24:12 +0100

    feat(exception): Added `operator<<` to `LoggerException`.

--- include/logger/Exceptions/LoggerException.h
+++ include/logger/Exceptions/LoggerException.h
@@ -1,6 +1,7 @@
 #ifndef SHOVOLOGGER_LOGGEREXCEPTION_H
 #define SHOVOLOGGER_LOGGEREXCEPTION_H
 
+#include <iostream>
 #include <stdexcept>
 
 namespace logger::exception
@@ -12,6 +13,15 @@ public:
     explicit LoggerException(const std::string& what_arg)
         : std::runtime_error(what_arg) {}
 };
+
+inline std::ostream &operator<<(
+    std::ostream& os,
+    const LoggerException& exception
+)
+{
+    os << exception.what() << std::endl;
+    return os;
+}
 
 }
 
