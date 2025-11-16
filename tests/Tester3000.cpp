55a444f1b8cdfcbb96b14f806996224d8501aa6d
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-16 13:37:24 +0100

    tests: Updated Tester3000.

--- tests/Tester3000.cpp
+++ tests/Tester3000.cpp
@@ -2,31 +2,40 @@
 #include "logger/Sinks/ConsoleSink.h"
 #include "logger/Sinks/JsonFileSink.h"
 #include "logger/Sinks/LogFileSink.h"
+#include "logger/Sinks/NdJsonFileSink.h"
 
-int main(const int argc, const char *argv[])
+int main(const int argc, const char* argv[])
 {
-    Logger::getInstance().addSink(std::make_shared<logger::ConsoleSink>());
-    Logger::getInstance().addSink(std::make_shared<logger::LogFileSink>(
-        Logger::generateLogFileName("Tester3000")));
-    Logger::getInstance().addSink(std::make_shared<logger::LogFileSink>("logs/latest.log"));
-    Logger::getInstance().addSink(std::make_shared<logger::JsonFileSink>("logs/latest.json"));
+    logger::sink::Settings sinkSettings{
+        .showOnlyTime = true,
+        .showThreadId = false,
+        .showSource = false,
+        .showColumnNumber = false
+    };
+
+    Logger::getInstance().addSink<logger::ConsoleSink>(sinkSettings);
+    // Logger::getInstance().addSink<logger::LogFileSink>(Logger::generateLogFileName("Tester3000", "log"));
+    Logger::getInstance().addSink<logger::LogFileSink>("logs/latest.log", sinkSettings);
+    // Logger::getInstance().addSink<logger::JsonFileSink>("logs/latest.json");
+    // Logger::getInstance().addSink<logger::JsonFileSink>("logs/badjson.txt");
+    // Logger::getInstance().addSink<logger::NdJsonFileSink>("logs/test.ndjson");
 
     Logger::initialize(
         "Tester3000",
         argc, argv,
         logger::BuildInfo::fromCMake(),
         logger::Settings(
-            logger::Level::kDebug,
-            true,
-            true,
-            false,
-            1,
-            0
+            logger::Level::kDebug//,
+            // 1,
+            // 0
         )
     );
 
     LOG_DEBUG("Debug log.");
     LOG_INFO("Info log.");
+
+    Logger::getInstance().addSink<logger::JsonFileSink>("logs/badjson2.txt");
+
     LOG_WARN("Warning log.");
     LOG_ERR("Error log.");
     LOG_CRIT("Critical log.");
